85b008b8e13c877853e2d679b73b17f2458ac269
RudyPrabowoRPL <32088532+RudyPrabowoRPL@users.noreply.github.com>
2017-11-21 14:27:28 +0700

    Add files via upload

---
+++ P10 Pewarisan dan Polimorfisme/Bentukwajah.java
@@ -0,0 +1,31 @@
+class Bentukwajah{
+    /** Membuat sebuah methd respons pada class induk dimana mengembalikan sebuah nilai String */
+    public String respons(){
+        return("Perhatikan reaksi wajah saya\n");
+    }
+}
+
+    /** Membuat class-class lain dengan turunan dari class Bentuhwajah */
+class Senyum extends Bentukwajah{
+    public String respons(){
+        return("Senyum karena senang\n");
+    }
+}
+
+class Tertawa extends Bentukwajah{
+    public String respons(){
+        return("Tertawa karena gembira\n");
+    }
+}
+
+class Marah extends Bentukwajah{
+    public String respons(){
+        return("Kemarahan disebabkan oleh bertengkar\n");
+    }
+}
+
+class Sedih extends Bentukwajah{
+    public String respons(){
+        return("Sedih disebabkan oleh cemburu \n");
+    }
+}
